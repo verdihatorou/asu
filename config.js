8826fa57d9d3203a9e22403b761a6ce92e76deca
Verdi hatoru <95448883+verdihatorou@users.noreply.github.com>
2022-09-05 11:23:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['6282221792667', 'ePro || RtXzy', true]
+  ['6285706735450', 'bot by verdi hatoru || xziro', true]
   // [owner bukan bot!]
 ] // paste nomor di sini
 global.mods = [] // bantuan?
@@ -20,9 +20,9 @@ global.APIKeys = { // APIKey Here
 }
 
 // Wm untuk stiker
-global.tio = '© Developer'
-global.packname = 'ePro Rtxzy'
-global.author = '© DayDay Happy'
+global.tio = '© dev'
+global.packname = 'fore'
+global.author = '© Xziro chan'
 
 global.multiplier = 70 // Semakin Tinggi levelmu semakin naik
 
